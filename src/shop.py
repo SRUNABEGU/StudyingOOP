62f5baee21ff7a6e06116f5674f9eaaadc2a4f11
ruslan <ruslanpetryakov@yandex.ru>
2026-04-10 16:19:49 +0800

    main содержит проверочный код, создан новый модуль shop для выполнения домашнего задания

---
+++ src/shop.py
@@ -0,0 +1,97 @@
+from abc import ABC, abstractmethod
+
+
+class MixinLog:
+    def __init__(self, *args, **kwargs):
+        print(f"{self.__class__.__name__}({', '.join([repr(a) for a in args])})")  # pragma: no cover
+        super().__init__() # pragma: no cover
+
+
+class BaseProduct(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        pass
+
+
+class Product(MixinLog, BaseProduct):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
+
+        super().__init__(name, description, price, quantity)
+
+    @classmethod
+    def new_product(cls, product_data: dict):
+        return cls(**product_data)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    def __add__(self, other):
+        if type(self) is not type(other):
+            raise TypeError("Можно складывать только товары одного класса")
+        return (self.price * self.quantity) + (other.price * other.quantity)
+
+
+class Smartphone(Product):
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+
+class Category:
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name: str, description: str, products: list = None):
+        self.name = name
+        self.description = description
+        self.__products = []
+        Category.category_count += 1
+        if products:
+            for product in products:
+                self.add_product(product)
+
+    def add_product(self, product):
+        if not isinstance(product, Product):
+            raise TypeError("Добавлять в категорию можно только объекты Product или его наследников")
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_strings = [str(product) for product in self.__products]
+        return "\n".join(product_strings)
+
+    def __str__(self):
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
+
